all bug fixed: type_mask should be shift-by-1 (learned lessons: softmax(-1e9*N) = 1/N, residual connection can be the cheating without proper masking

Utils.py
@@ -62,7 +62,7 @@ def log_likelihood(model, data, time, types):
 
     type_mask = torch.zeros([*types.size(), model.num_types], device=data.device)
     for i in range(model.num_types):
-        type_mask[:, :, i] = (types == i + 1).bool().to(data.device)
+        type_mask[:, :-1, i] = (types[:, 1:] == i + 1).bool().to(data.device)
 
     all_hid = model.linear(data)
     all_lambda = softplus(all_hid, model.beta)